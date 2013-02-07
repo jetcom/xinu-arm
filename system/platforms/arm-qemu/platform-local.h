692bb0259b4d9c988841919116dbb8e5216d3b5a
David Larsen <dcl9934@cs.rit.edu>
2013-02-06 21:00:52 -0500

    arm-qeum: Use the default stack size
    Because we have a lot more ram than the fluke-arm port had, we can
    afford to use the default platform-independent stack size.

--- system/platforms/arm-qemu/platform-local.h
+++ system/platforms/arm-qemu/platform-local.h
@@ -6,10 +6,5 @@
 #ifndef _ARM_QEMU_PLATFORM_LOCAL_H
 #define _ARM_QEMU_PLATFORM_LOCAL_H
 
-/*
- * The fluke-arm platform has so little memory that the global default
- * of 64k for INITSTK is too big. Try something more conservativer.
- */
-#define INITSTK (1337)
 
 #endif /* _ARM_QEMU_PLATFORM_LOCAL_H */
