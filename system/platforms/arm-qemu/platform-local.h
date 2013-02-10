597222ab8bc4c4d7521cf62a96f9f560fd9a3b88
David Larsen <dcl9934@cs.rit.edu>
2013-02-10 14:48:09 -0500

    Sloppy workaround for the VIC problem in QEMU.
    In some versions of QEMU, there's an off-by-one error in the vectored
    interrupt controller (specifically, the PL190). This was fixed in
    September 2012, but many systems have not updated to that fix yet.
    
    The workaround is to figure out the source of interrupt and jump to the
    correct handler manually. This is a lot more work than just reading from
    VIC_VEC_ADDR and jumping, but should work in systems which do and do not
    have VICs with the off-by-one error.
    
    Bugs:
    
    * The manual fallback only happens if the interrupt handler is null, so
      weird stuff will happen if we have two IRQs next to each-other
      enabled (because there will still be an off-by-one error, but we won't
      detect it). We should either auto-detect the bug or always do the
      workaround.
    
    * This change doesn't handle interrupt priorization properly. In fact,
      it services interrupts in the reverse order that it's supposed to! It
      works fine for single interrupts.

--- system/platforms/arm-qemu/platform-local.h
+++ system/platforms/arm-qemu/platform-local.h
@@ -9,4 +9,10 @@
 /* Memory-mapped location for the vectored interrupt controller. */
 #define VIC_BASE_ADDRESS 0x10140000
 
+/* In some versions of QEMU, the VIC has an off-by-one error and gives us a
+ * null ISR. This was fixed in September, 2012, so not all systems have
+ * upgraded to a version with the fix.
+ */
+#define QEMU_VIC_WORKAROUND 1
+
 #endif /* _ARM_QEMU_PLATFORM_LOCAL_H */
